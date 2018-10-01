9be862d9a894c23c485128f3190036acaf06a29c
Raf <bachourian@gmail.com>
2018-10-01 17:12:03 +0200

    cursor bugged

--- Démineur.c
+++ Démineur.c
@@ -9,13 +9,18 @@ void aff_TTMines (TTMines* T);
 
 
 int main () {
-    char instr;
-    int lin=1,col=1;
+    char dir;
     TTMines* T;
 
     T=init_TTMines(T,"");
+    while (dir!='p'){
+        fflush(stdin);
+        dir=getchar();
+        deplace_curseur (T,dir);
+        system("clear");
+        aff_TTMines(T);
+    }
     
-    aff_TTMines(T);
     free_TTMines(T);
     //aff_TTMines(T);
     exit(0);
--- structure.c
+++ structure.c
@@ -3,12 +3,13 @@
 typedef struct {
     //Le tableau 2d de mines allant de [1][1] à [n][n]
     int ** TMines;
-    int n;
+    int n,lin,col;
 } TTMines;
 
 TTMines* init_TTMines (TTMines* T ,char* difficulte){
     int nbombe;
-    T=(TTMines*) malloc(sizeof(TTMines));
+    T=malloc(sizeof(TTMines));
+    T->lin=T->col=1;
     if (difficulte=="facile") {
         (*T).n=10;
         nbombe=10;
@@ -24,10 +25,10 @@ TTMines* init_TTMines (TTMines* T ,char* difficulte){
         nbombe=10;
     }
     //Allocation de mémoire
-    T->TMines=(int **)malloc((T->n+2)*sizeof(int*));
+    T->TMines=malloc((T->n+2)*sizeof(int*));
 
     for (int i = 0; i < ((*T).n)+2; i++) {
-        T->TMines[i]=(int*) malloc((T->n+2)*sizeof(int));
+        T->TMines[i]= malloc((T->n+2)*sizeof(int));
     }
     //Initialisation à 0
 
@@ -51,14 +52,37 @@ void free_TTMines(TTMines* T){
 }
 
 void aff_TTMines (TTMines* T){
-    for(int i = 0; i < (T->n)+2; i++) {
-        for(int j = 0; j < T->n+2; j++) {
-            
-            if (T->TMines[i][j]==0) {
-                printf("%c ",0x10);;
+    for(int i = 1; i <= (T->n); i++) {
+        for(int j = 1; j <= T->n; j++) {
+            if (i==T->lin && j==T->col){
+                printf("* ");
+            } else if (T->TMines[i][j]==0) {
+                printf("%c ",0x10);
             }else 
-                printf("%d ",T->TMines[i][j]);;
+                printf("%d ",T->TMines[i][j]);
         }
         printf("\n");
+    }
+}
+
+void deplace_curseur (TTMines * T,char dir){
+    switch (dir){
+        case 'z': 
+            if (T->lin>1)
+                T->lin--;
+            break;
+        case 'q': 
+            if (T->col>1)
+                T->lin--;
+            break;
+        case 's': 
+            if (T->lin < T->n-1)
+                T->lin--;
+            break;
+        case 'd': 
+            if (T->col < T->n-1)
+                T->lin--;
+            break;
+
     }
 }
